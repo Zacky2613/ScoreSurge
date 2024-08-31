29812318b2c0e05a712752adfdf4b32a64b3d9de
Zacky2613 <zackycuber@gmail.com>
2024-08-31 21:45:11 +1000

    first commit

---
+++ Scoresurge/api.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
+from datetime import datetime
+
+
+# Flask config.
+app = Flask(__name__, template_folder="templates")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///.database.db"
+
+# Database config.
+db = SQLAlchemy(app)
+
+
+api = Api(app)
+
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    page_id = db.Column(db.String(50), nullable=False, unique=True)
+    content = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self) -> str:
+        return f"Content: {self.content}"
+
+
+
+
+
+
+
+
+
+# Defining multiple routes for the same function.
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/notes/<string:page_id>", methods=["POST", "GET"])
+def notes(page_id):
+    
+    note = Notes.query.filter_by(page_id=page_id).first()
+    
+    if (request.method == "POST"):
+        note_content = request.form['content']
+
+        print(note_content)
+
+        if note:
+            note.content = note_content
+        else:
+            note = Notes(page_id=page_id, content=note_content)
+            db.session.add(note)
+
+        db.session.commit()
+        return redirect(url_for('notes', page_id=page_id))
+
+
+    return render_template("notes.html", note=note, page_id=page_id)    
+
+
+        
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    
+    app.run(debug=True)
---
+++ Scoresurge/static/js/script.js
@@ -0,0 +1,106 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Becuase there's more than one dropdown mean, must use all.
+    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+
+    // Loop through each dropdown toggle
+    dropdownToggles.forEach(toggle => {
+        const dropdown = toggle.parentElement;
+        const dropdownContent = dropdown.querySelector('.dropdown-content');
+
+        toggle.addEventListener('click', function() {
+            // Toggle the active class
+            dropdown.classList.toggle('active');
+
+            // Adjust the dropdown content's max height for smooth transition
+            if (dropdown.classList.contains('active')) {
+                dropdownContent.style.maxHeight = dropdownContent.scrollHeight + "px";
+            } else {
+                dropdownContent.style.maxHeight = 0;
+            }
+        });
+    });
+});
+
+
+// This is for the textboxes in the NOTES area
+
+// Changes fonts with the selector
+function changeFont(font) {
+    if (font) {
+        // It's depracted but it works so idc
+        document.execCommand("fontName", false, font);
+    }
+}
+
+function formatText(command) {
+    document.execCommand(command, false, null);
+}
+
+
+// MY OWN JS | IT WORKS YIPPIE
+
+const format_button = document.querySelectorAll("#text-format-btn");
+
+format_button.forEach(button => {
+    button.addEventListener("click", function(event) {
+        button.classList.toggle("active")
+    });
+});
+
+
+// If ctrl+s is pressed it'll save.
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 's') {
+
+        // Preven the broswer save .html thingy
+        event.preventDefault();
+
+        // Without this, the save is blank
+        document.getElementById('note-content').value = document.getElementById('editor').innerHTML;
+        document.getElementById('note-form').submit(); // Trigger form submission
+    }
+});
+
+
+// Handling chaning toolbar width when textbox one changes from user resize
+function adjustToolbar() {
+    const editor = document.getElementById("editor");
+    const toolbar = document.querySelector(".toolbar");
+
+    // Changing the toolbar width
+    // Why the -20? I honestly don't know, it stops the overhand with the toolbar
+    toolbar.style.width = editor.clientWidth-20 + "px";
+}
+
+// Function to handle resizing of the editor
+function handleResize() {
+    const editor = document.getElementById("editor");
+
+    // ResizeObserver() is to watch for changes in size, passed through
+    // as any argument
+    let resise_observer = new ResizeObserver(() => {
+        adjustToolbar();
+    });
+
+    // Like here
+    resise_observer.observe(editor);
+}
+
+// On page load initalize a function to be 
+window.onload = function() {
+    handleResize();
+};
+
+// Also, handle cases where the window is resized
+window.onresize = function() {
+    adjustToolbar();
+};
+
+document.getElementById('note-form').addEventListener('submit', function() {
+    const editorContent = document.getElementById('editor').innerHTML;
+
+    document.getElementById('note-content').value = editorContent;
+});
