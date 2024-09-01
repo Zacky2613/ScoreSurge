a58fc9a8c782cea1cb6c9d09baf1b2154f5079a7
Zacky2613 <zackycuber@gmail.com>
2024-09-01 22:00:30 +1000

    Added functionality to adding notes/classes
    - Add new Classes database model
    - Added injecting data into base.html to pass Notes and Classes, will be used in future
    - You can now create classes/notes using the new + icon.
    - Modals are used UI wise for creating new classes.
    - Added new database value, class/note_name. This is to seperate from page_id for UI display

--- Scoresurge/api.py
+++ Scoresurge/api.py
@@ -17,12 +17,22 @@ api = Api(app)
 
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    note_name = db.Column(db.String(50), nullable=False, unique=True)
     page_id = db.Column(db.String(50), nullable=False, unique=True)
     content = db.Column(db.Text)
-    created = db.Column(db.DateTime, default=datetime.utcnow)
     
     def __repr__(self) -> str:
         return f"Content: {self.content}"
+
+
+class Classes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    class_name = db.Column(db.String(50), nullable=False, unique=True)
+    page_id = db.Column(db.String(50), nullable=False, unique=True)
+    content = db.Column(db.Text)
+    
+    def __repr__(self) -> str:
+        return f"Class: {self.content}"
 
 
 # Defining multiple routes for the same function.
@@ -32,9 +42,32 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/create_note_and_class", methods=["POST"])
+def create_class():
+    note_and_class_title = request.form["class_name"]
+
+    page_id = note_and_class_title.lower().replace(" ", "_")
+    note_and_class_title_clean = page_id.replace("_", " ").title()
+    
+    existing_class = Notes.query.filter_by(page_id=page_id).first()
+
+    # If the new class is unqiue
+    if not existing_class:
+        new_class = Classes(class_name=note_and_class_title_clean, page_id=page_id)
+        db.session.add(new_class)
+
+        new_note = Notes(note_name=note_and_class_title_clean, page_id=page_id, content="")
+        db.session.add(new_note)
+        
+        db.session.commit()
+        
+    return redirect(url_for('notes', page_id=page_id))
+
+
+
+# Note updating
 @app.route("/notes/<string:page_id>", methods=["POST", "GET"])
 def notes(page_id):
-    
     note = Notes.query.filter_by(page_id=page_id).first()
     
     
@@ -45,9 +78,12 @@ def notes(page_id):
 
         if note:
             note.content = note_content
+            
         else:
-            note = Notes(page_id=page_id, content=note_content)
+            note_name = page_id.replace("_", " ").title()
+            note = Notes(note_name=note_name, page_id=page_id, content=note_content)
             db.session.add(note)
+            
 
         db.session.commit()
         return redirect(url_for('notes', page_id=page_id))
@@ -56,10 +92,23 @@ def notes(page_id):
     return render_template("notes.html", note=note, page_id=page_id)    
 
 
-        
+@app.context_processor
+def inject_data():
+    notes = Notes.query.all()
+    classes = Classes.query.all() 
+    
+    data = {
+        "notes": notes,
+        "classes": classes,
+        "username": "John Doe"
+    }
+    
+    return data
+
 
 if __name__ == "__main__":
     with app.app_context():
+        # db.drop_all()
         db.create_all()
     
     app.run(debug=True)
--- Scoresurge/static/js/script.js
+++ Scoresurge/static/js/script.js
@@ -1,4 +1,28 @@
 
+var modal = document.getElementById("classModal");
+
+var btn = document.getElementById("add-btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+document.getElementById("add-btn").onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
 
