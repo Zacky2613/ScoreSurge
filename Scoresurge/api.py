610fda80caf85a22231bab4f207745f57eb27161
Zacky2613 <zackycuber@gmail.com>
2024-09-05 23:19:04 +1000

    Added classes, ability to make class planner.

--- Scoresurge/api.py
+++ Scoresurge/api.py
@@ -20,7 +20,7 @@ class Notes(db.Model):
     note_name = db.Column(db.String(50), nullable=False, unique=True)
     page_id = db.Column(db.String(50), nullable=False, unique=True)
     content = db.Column(db.Text)
-    
+
     def __repr__(self) -> str:
         return f"Content: {self.content}"
 
@@ -29,10 +29,26 @@ class Classes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     class_name = db.Column(db.String(50), nullable=False, unique=True)
     page_id = db.Column(db.String(50), nullable=False, unique=True)
-    content = db.Column(db.Text)
     
     def __repr__(self) -> str:
-        return f"Class: {self.content}"
+        return f"Schedule: {self.content}"
+
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    week_number = db.Column(db.Integer)
+    day_of_week = db.Column(db.String(20)) 
+    class_name = db.Column(db.Text)
+    content_title = db.Column(db.String(60))
+    content = db.Column(db.Text)
+    
+    # teacher = db.Column(db.String(40))
+    # TODO = db.Column(db.String(40), nullable=True)
+    
+    
+    def __repr__(self) -> str:
+        return f"Schedule: {self.content}"
+
 
 
 # Defining multiple routes for the same function.
@@ -40,6 +56,41 @@ class Classes(db.Model):
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/classes/<string:page_id>")
+def classes(page_id):
+    class_data = Classes.query.filter_by(page_id=page_id).first()
+    schedule_data = Schedule.query.filter_by(class_name=class_data.class_name).all()
+    
+    return render_template(
+        "classes.html", 
+        class_data=class_data, 
+        schedule=schedule_data, 
+        page_id=page_id
+    )
+
+
+
+
+@app.route("/create_class_planner/<string:page_id>", methods=["POST"])
+def create_class_planner(page_id):
+    class_data = Classes.query.filter_by(page_id=page_id).first()
+    
+    class_times = request.form["days_of_week"]
+    clean_class_times = class_times.split("|")
+    
+    x = 0
+    for item in clean_class_times:
+        day = item.replace(" ", "").replace("yP", "y P")
+
+        new_day = Schedule(class_name=class_data.class_name, day_of_week=day)
+        db.session.add(new_day)
+        
+        db.session.commit()
+        x += 1
+
+    return redirect(url_for('classes', page_id=page_id))
+
 
 
 @app.route("/create_note_and_class", methods=["POST"])
@@ -61,14 +112,14 @@ def create_class():
         
         db.session.commit()
         
-    return redirect(url_for('notes', page_id=page_id))
-
+    return redirect(url_for('classes', page_id=page_id))
 
 
 # Note updating
 @app.route("/notes/<string:page_id>", methods=["POST", "GET"])
 def notes(page_id):
     note = Notes.query.filter_by(page_id=page_id).first()
+    
     
     
     if (request.method == "POST"):
@@ -78,7 +129,7 @@ def notes(page_id):
 
         if note:
             note.content = note_content
-            
+
         else:
             note_name = page_id.replace("_", " ").title()
             note = Notes(note_name=note_name, page_id=page_id, content=note_content)
@@ -87,19 +138,27 @@ def notes(page_id):
 
         db.session.commit()
         return redirect(url_for('notes', page_id=page_id))
+    
+    # If the note the user is trying to access don't exist.
+    if not note:
+        return "You haven't created this note, please do so by pressing + next to classes", 404
 
 
     return render_template("notes.html", note=note, page_id=page_id)    
+
+
 
 
 @app.context_processor
 def inject_data():
     notes = Notes.query.all()
     classes = Classes.query.all() 
+    schedule = Schedule.query.all() 
     
     data = {
         "notes": notes,
         "classes": classes,
+        "schedule": schedule,
         "username": "John Doe"
     }
     
